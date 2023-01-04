ba2cfc9918f12866f50eb61509fe4ad7140ed986
Crimson5768 <tommy.long7548@gmail.com>
2023-01-04 12:49:05 -0800

    removed keep_alive. Added connect and disconnect

--- cogs/music.py
+++ cogs/music.py
@@ -5,6 +5,8 @@ import asyncio
 from discord.ext import commands
 from database_user import host, user, passwd
 import mysql.connector
+import os
+import youtube_dl
 
 class Music(commands.Cog):
     def __init__(self, bot):
@@ -31,14 +33,18 @@ class Music(commands.Cog):
         # SQL command to show all databases currently made. It fetches a list of all databases that exist currently
         # Creates a database that contains inspiring quotes if it doesn't exist in the list of databases
         if self.DATABASE_NAME not in self.generateList("SHOW DATABASES"):
+            print("Creating database...")
             self.curs.execute(f"CREATE DATABASE {self.DATABASE_NAME}")
+            print("Success!")
 
         # Navigate to inspiringdatabase and show all tables in that database
         self.curs.execute(f"USE {self.DATABASE_NAME}")
 
         # Check if tuple exists in table
         if self.TABLE not in self.generateList("SHOW TABLES"):
+            print("Creating table...")
             self.curs.execute(f"CREATE TABLE {self.TABLE} ({self.ID} INT AUTO_INCREMENT PRIMARY KEY, {self.MUSIC_LINK} VARCHAR(255), {self.SONG_NUMBER} INT)")
+            print("Success!")
 
     def generateList(self, sql):
         '''
@@ -65,31 +71,42 @@ class Music(commands.Cog):
 
         return len(self.generateList(sql)) + 1
 
+    def endSong(self, guild, path):
+        os.remove(path)
+
     @commands.command()
     async def join(self, ctx):
         '''
         Bot joins a voice channel the user is currently in
-
-         
         '''
-        await ctx.channel.send("```Coming Soon!```")
+        try:
+            # Get channel that user is currently in
+            channel = ctx.author.voice.channel
+            
+            # Connect bot to VC user is in
+            await channel.connect()
+
+            await ctx.channel.send(f"```Now joining: {channel}!```")
+
+        except:
+            await ctx.channel.send(f"```CrimBot is already in: {ctx.author.voice.channel}!```")
     
     @commands.command()
     async def disconnect(self, ctx):
         '''
         Bot disconnects from a voice channel it was in
-
-        
         '''
-        await ctx.channel.send("```Coming Soon!```")
-    
+        try:
+            await ctx.voice_client.disconnect()
+            await ctx.channel.send(f"```Now leaving: {ctx.author.voice.channel}!```")
+
+        except:
+            await ctx.channel.send(f"```CrimBot is not in a voice channel currently!```")                                   
+
     @commands.command()
-    async def youtube(self, ctx, *, msg):
-        '''
-        Bot plays a youtube video in the voice channel
-        '''
+    async def play(self, ctx, url):
         await ctx.channel.send("```Coming Soon!```")
-
+        
     @commands.command()
     async def pause(self, ctx):
         '''
@@ -111,17 +128,24 @@ class Music(commands.Cog):
         '''
         await ctx.channel.send("```Coming Soon!```")
 
-    @commands.command()
+    @commands.command(command = "del")
     async def deleteSong(self, ctx, index):
         '''
         Delete a song from the music table
         '''
         await ctx.channel.send("```Coming Soon!```")
     
-    @commands.command()
+    @commands.command(command = "dis")
     async def displaySongs(self, ctx):
         '''
         Display all songs in queue
+        '''
+        await ctx.channel.send("```Coming Soon!```")
+    
+    @commands.command(command = "addsong")
+    async def addSong(self, ctx):
+        '''
+        Add a song to the queue
         '''
         await ctx.channel.send("```Coming Soon!```")
 
