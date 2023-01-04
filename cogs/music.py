91924d170114f55622fdd6bf80790b68dcc608f3
Crimson5768 <tommy.long7548@gmail.com>
2023-01-04 00:34:41 -0800

    initialized database

--- cogs/music.py
+++ cogs/music.py
@@ -3,56 +3,133 @@ import json
 import discord
 import asyncio
 from discord.ext import commands
+from database_user import host, user, passwd
+import mysql.connector
 
-'''
-THOUGHT PROCESS:
-    - When a play command is initated, the bot has to join a voice channel that the user is currently in.
-    - the youtube link that is requested to be played is added to the database queue
-    - when a song ends or a skip command is issued, the database deletes the song it 
-      just played and goes to the next song
-'''
 class Music(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.db = mysql.connector.connect(host = host, user = user, passwd = passwd)
+        self.DATABASE_NAME = "music_db"
+        self.TABLE = "song_playlist"
+        self.ID = "id"
+        self.MUSIC_LINK = "music_link"
+        self.SONG_NUMBER = "song_number"
+        
+        self.curs = self.db.cursor()
+
+        self.generate_db()
+
+        self.musicCount = self.update_count()
+    
+    def generate_db(self):
+        '''
+        Creates a database called "music_db" if it doesn't exist and a table called "song_playlist" if it doesn't exist
+
+        :return: None
+        '''
+        # SQL command to show all databases currently made. It fetches a list of all databases that exist currently
+        # Creates a database that contains inspiring quotes if it doesn't exist in the list of databases
+        if self.DATABASE_NAME not in self.generateList("SHOW DATABASES"):
+            self.curs.execute(f"CREATE DATABASE {self.DATABASE_NAME}")
+
+        # Navigate to inspiringdatabase and show all tables in that database
+        self.curs.execute(f"USE {self.DATABASE_NAME}")
+
+        # Check if tuple exists in table
+        if self.TABLE not in self.generateList("SHOW TABLES"):
+            self.curs.execute(f"CREATE TABLE {self.TABLE} ({self.ID} INT AUTO_INCREMENT PRIMARY KEY, {self.MUSIC_LINK} VARCHAR(255), {self.SONG_NUMBER} INT)")
+
+    def generateList(self, sql):
+        '''
+        Execute a SQL command and generate rows from that given table based on certain conditions
+
+        :param sql: SQL command to be executed
+        :return: List
+        '''
+        self.curs.execute(sql)
+
+        # A list of rows
+        lst = self.curs.fetchall()  
+        
+        # Since the list returns tuple elements, we access the quote using item[0] since its a (quote, ) format
+        return [item[0] for item in lst]
+
+    def update_count(self):
+        '''
+        Updates quoteCount to match how many quotes currently exist in the database
+
+        :return: None
+        '''
+        sql = f'SELECT * FROM {self.TABLE}'
+
+        return len(self.generateList(sql)) + 1
 
     @commands.command()
     async def join(self, ctx):
-        connected = ctx.author.voice
-      
-        if connected:
-            await connected.channel.connect()
+        '''
+        Bot joins a voice channel the user is currently in
+
+         
+        '''
+        await ctx.channel.send("```Coming Soon!```")
     
     @commands.command()
     async def disconnect(self, ctx):
+        '''
+        Bot disconnects from a voice channel it was in
 
-        # Disconnect bot from vc
-        await ctx.voice_client.disconnect()
+        
+        '''
+        await ctx.channel.send("```Coming Soon!```")
     
     @commands.command()
     async def youtube(self, ctx, *, msg):
-      pass
+        '''
+        Bot plays a youtube video in the voice channel
+        '''
+        await ctx.channel.send("```Coming Soon!```")
 
     @commands.command()
     async def pause(self, ctx):
-        await ctx.voice_channel.pause()
-        await ctx.send("Paused")
-    
+        '''
+        Bot pauses youtube video
+        '''
+        await ctx.channel.send("```Coming Soon!```")
+        
     @commands.command()
     async def resume(self, ctx):
-        await ctx.voice_channel.resume()
-        await ctx.send("Now Resuming!")
+        '''
+        Bot resumes to play youtube video after it got paused
+        '''
+        await ctx.channel.send("```Coming Soon!```")        
 
     @commands.command()
     async def skip(self, ctx):
-        pass
+        '''
+        Bot skips current youtube video and goes to the next video in queue
+        '''
+        await ctx.channel.send("```Coming Soon!```")
 
     @commands.command()
     async def deleteSong(self, ctx, index):
-        pass
+        '''
+        Delete a song from the music table
+        '''
+        await ctx.channel.send("```Coming Soon!```")
     
     @commands.command()
     async def displaySongs(self, ctx):
-        pass
+        '''
+        Display all songs in queue
+        '''
+        await ctx.channel.send("```Coming Soon!```")
 
 async def setup(bot):
+    """
+    setup function that lets the bot register the cog that was made up above to be used
+
+    :param bot: Discord Bot object that will interact with the Inspire category of commands.
+    :return: None
+    """
     await bot.add_cog(Music(bot))
