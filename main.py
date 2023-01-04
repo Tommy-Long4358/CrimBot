71285f6cf2d3830268c8d7df6fa54b1ec8165d64
Crimson5768 <tommy.long7548@gmail.com>
2023-01-03 18:47:47 -0800

    removed replit key

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ import discord
 import random
 import asyncio
 from keep_alive import keep_alive
+from token_key import token
 from discord.ext import commands
 
+'''
+THINGS TO DO:
+    1. Transport bot from replit to VSC and figure out how to keep it live 24/7
+    2. Music playing command
+    3. Copypasta creation command
+    4. Make a website for the bot
+    5. Add any other commands needed
+'''
+  
 # Intents is for permissions that the bot has access to
 intents = discord.Intents.default()
 
@@ -14,28 +24,25 @@ intents.message_content = True
 # Defining a Discord Client with intents and the "!" command prefix
 bot = commands.Bot(command_prefix = "!", intents = intents)
 
-
 # The @bot.event() decorator is used to register an event
 @bot.event
 async def on_ready():
     """
     Event for bot connecting to Discord. When the code is compiled, the 
-    bot boots up using this async function
+    bot boots up using this async function.
 
     :return: None
     """
     print(f'{bot.user} has connected to Discord!')
   
-
 @bot.event
 async def on_message(message):
     """
-    Event for bot to recognize certain keywords in a message the user sends
+    Event for bot to recognize certain keywords in a message the user sends.
 
     :param message: a message from the user
     :return: None
     """
-
     # Prevent bot from responding to itself
     if message.author == bot.user:
         return
@@ -52,7 +59,6 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
-    
 @bot.command()
 async def roll(ctx, arg1, arg2):
     """
@@ -64,7 +70,6 @@ async def roll(ctx, arg1, arg2):
     :param arg2: the greatest number range input
     :return: None
     """
-
     # Generate a random number
     number = random.randint(int(arg1), int(arg2))
 
@@ -76,15 +81,14 @@ async def load_extensions():
 
     :return: None
     """
-    for filename in os.listdir('./cogs'):
+    for filename in os.listdir('.\cogs'):
         if filename.endswith('.py'):
             await bot.load_extension(f'cogs.{filename[:-3]}')
-  
-# Token key for CrimBot so that the client knows which bot to run it on
-my_secret = os.environ['secret_key']
 
 asyncio.run(load_extensions())
 
 # Keeps the bot running 24/7
-keep_alive()
-bot.run(my_secret)
+#keep_alive()
+
+# Token key for CrimBot so that the client knows which bot to run it on
+bot.run(token)
